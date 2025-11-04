6058c82075650dae9bc8c97aa4b998a57fef4646
Miminimi234 <miraclejames812@gmail.com>
2025-11-04 02:43:58 +0100

    fix

---
+++ app/services/enhanced_judges.py
@@ -0,0 +1,7 @@
+"""
+Enhanced judicial panel orchestrator used by the API.
+"""
+
+class EnhancedJudicialPanel:
+    async def hear_case(self, case_facts: str, jurisdiction: str, case_type: str):
+        raise NotImplementedError
